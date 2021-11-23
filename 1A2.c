13197033d2e1823a7aeb4dc2ad9e6260d3c62a47
Goyalrahul1516 <87524423+Goyalrahul1516@users.noreply.github.com>
2021-11-23 12:31:01 +0530

    Add files via upload

---
+++ 1A2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int a;
+	printf("enter the number ");
+	scanf("%d",&a);
+	if(a%2==0)
+	{
+	printf("Even");
+	}
+	else
+	{
+		printf("Odd");
+	}
+
+ return 0;
+}
+
