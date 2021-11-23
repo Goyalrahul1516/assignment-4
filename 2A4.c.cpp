551964d8682ca918cfac9e22cf81bea803998c7b
Goyalrahul1516 <87524423+Goyalrahul1516@users.noreply.github.com>
2021-11-23 12:51:52 +0530

    Add files via upload

---
+++ 2A4.c.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+	int year;
+	printf("Enter year ");
+	scanf("%d",&year);
+	if(year%4==0)
+	{
+		if(year%100==0)
+		{
+			if(year%400==0)
+			{
+				printf("Year is Leap Year");
+			
+			}else
+			{
+				printf("Year is not Leap year");
+			}
+		}
+		else
+			{
+				printf("Year is not Leap year");
+			}
+	}
+	else
+			{
+				printf("Year is not Leap year");
+			}
+
+ return 0;
+}
+
